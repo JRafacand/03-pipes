1e1fffb7b96dda5be739289fcf9d16d41855a165
JRafacand <r.candilejo@gmail.com>
2023-02-27 23:39:34 -0500

    Utilizar Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: string='Batman';
+  nombre     :string  ='Batman';
+  PI         :number  =Math.PI;
+  porcentaje :number  =0.235;
+  salario    :number  =1234.5;
+  fecha      :Date    =new Date();
+  idioma     :string='es';
   array =[1,2,4,4,5,6,7,8,9,10];
-  PI:number=Math.PI;
+  valorpro=new Promise<string>((resolve) =>{
+    setTimeout(() => {
+      resolve('llego la data')  
+    }, 4500);
+  });
+
+  heroe={
+    nombre:'Bruce W',
+    clave:'BATMAN',
+    EDAD:45,
+    direccion:{
+      calle:'Primera',
+      casa:20
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { registerLocaleData } from '@angular/common';
+import  localeEs from '@angular/common/locales/es';
+import  localeFr from '@angular/common/locales/fr';
+import  localeGr from '@angular/common/locales/de';
 import { AppComponent } from './app.component';
 
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
+registerLocaleData(localeGr);
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:LOCALE_ID,
+      useValue:'es'
+      
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
