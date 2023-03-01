ad22ab443d602cc61e8bef7602eaf16fe13c4ca6
JRafacand <r.candilejo@gmail.com>
2023-03-01 15:00:47 -0500

    Cambio solucion Rafa

--- src/app/pipes/asteris.pipe.ts
+++ src/app/pipes/asteris.pipe.ts
@@ -6,11 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AsterisPipe implements PipeTransform {
 
   transform(value: string, todas:boolean=true): string {
-   return (todas)?'*'.repeat(value.length):value;
-
+   //return (todas)?'*'.repeat(value.length):value;
+   //let nombre=value;
+   if(todas){
+   return value.replace(/[a-zA-Z@0-9  ]/g, '*');
+   }
+   return value;
+}
 }
 
-}
 
 
 
